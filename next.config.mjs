1b9e6ab4eb0bddc22b808ba5277699929af7f2eb
Oleg Gorbatiuk <newgorbole@gmail.com>
2026-04-21 13:57:04 +0200

    Add redirects from organizers to partners

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,25 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "via.placeholder.com", "images.unsplash.com", "avatar.vercel.sh", "placehold.co"]
   },
+  async redirects() {
+    return [
+      {
+        source: "/organizer/:organizerId",
+        destination: "/partner/:organizerId",
+        permanent: true,
+      },
+      {
+        source: "/workshops/organizer/:organizerId",
+        destination: "/workshops/partner/:organizerId",
+        permanent: true,
+      },
+      {
+        source: "/retreats/organizer/:organizerId",
+        destination: "/retreats/partner/:organizerId",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
