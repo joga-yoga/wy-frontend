88324103b8b1605dc66cfaee0939fa7490e15861
Oleg Gorbatiuk <newgorbole@gmail.com>
2026-06-22 14:46:29 +0200

    Turn off mixpanel on local env

--- src/lib/mixpanelClient.ts
+++ src/lib/mixpanelClient.ts
@@ -7,8 +7,8 @@ const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
 const MIXPANEL_MODE = process.env.NEXT_PUBLIC_MIXPANEL_MODE;
 
 export const initMixpanel = () => {
-  if (!MIXPANEL_TOKEN) {
-    console.warn("Mixpanel token is missing! Check your .env file.");
+  if (!MIXPANEL_TOKEN || !MIXPANEL_MODE) {
+    console.warn("Mixpanel token or mode is missing! Check your .env file.");
     return;
   }
   mixpanel.init(MIXPANEL_TOKEN, {
