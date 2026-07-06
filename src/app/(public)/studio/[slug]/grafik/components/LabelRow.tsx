fbb1c347a57e383236aed31b431167666710b51a
Oleg Gorbatiuk <newgorbole@gmail.com>
2026-07-06 13:22:53 +0200

    Add static LabelRow for DayStrip

---
+++ src/app/(public)/studio/[slug]/grafik/components/LabelRow.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+import { buildWeekDays, getLabelColorClass } from "./dayStripUtils";
+
+interface LabelRowProps {
+  weekStart: Date;
+  todayStr: string;
+}
+
+export function LabelRow({ weekStart, todayStr }: LabelRowProps) {
+  const days = buildWeekDays(weekStart, [], todayStr);
+
+  return (
+    <div className="flex justify-between gap-1">
+      {days.map((day) => (
+        <span
+          key={day.date}
+          className={cn(
+            "min-w-[40px] px-1.5 text-center text-xs font-medium uppercase",
+            getLabelColorClass(day),
+          )}
+        >
+          {day.dayLabel}
+        </span>
+      ))}
+    </div>
+  );
+}
