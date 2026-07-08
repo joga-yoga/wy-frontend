205806b7b0ca4818c31c1ab12b5681cea3b409ad
Oleg Gorbatiuk <newgorbole@gmail.com>
2026-07-08 16:58:11 +0200

    Fix typo

--- src/app/(public)/studio/[slug]/schedule/SessionDetailDrawer.tsx
+++ src/app/(public)/studio/[slug]/schedule/SessionDetailDrawer.tsx
@@ -1108,7 +1108,7 @@ export function SessionDetailDrawer({
 
           {isReady && detail ? (
             <>
-              <div className="flex min-h-0 flex-1 flex-col pt-12˝">
+              <div className="flex min-h-0 flex-1 flex-col pt-12">
                 {isCancelled && <CancelledBanner />}
                 {isBooked && !isCancelled && <BookedBanner booking={detail.viewer_booking} />}
                 {isBooked && !isCancelled && changeVisible && (
